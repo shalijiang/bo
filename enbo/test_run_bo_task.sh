a29aaff95d248d23edcab5b84194856a735bb59b
Shali Jiang <shalijiang@gmail.com>
2020-12-06 21:05:06 -0800

    test_run_bo_task.sh

--- enbo/test_run_bo_task.sh
+++ enbo/test_run_bo_task.sh
@@ -1,7 +1,7 @@
-for func in branin ackley5
+for func in branin 
 do
-    for method in EI 2.wsms.gh.1 3.wsms.gh.1
+    for method in random EI 2.wsms.gh.1 3.wsms.1 2.EI.sample 2.rts.3 3.wspms.1 2.rollout.1 2.glasses
     do
-        python run_bo_task.py $func $method 2 2 1 debug_results log 
+        python run_bo_task.py $func $method 1 2 1 debug_results log 
     done
 done
